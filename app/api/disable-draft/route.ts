abd956920b49f9376e779526c62ef66a24049525
coldlavaai <otatler@gmail.com>
2025-10-18 19:05:55 +0100

    Add route to disable draft mode

--- app/api/disable-draft/route.ts
+++ app/api/disable-draft/route.ts
@@ -2,9 +2,6 @@ import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 export async function GET(request: Request) {
-  // Disable draft mode
   draftMode().disable()
-
-  // Redirect to the homepage
   redirect('/')
 }
